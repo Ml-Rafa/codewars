42c7047b4aa2a94a77d0c611023992ad5ffaf878
Ml-Rafa <rafael.aoliveira@mercadolivre.com>
2023-01-15 16:06:37 -0300

    initial challenge 01

---
+++ scripts_in_ruby/desafio_02.rb
@@ -0,0 +1,9 @@
+Conclua o método/função para que ele converta palavras delimitadas por traço
+sublinhado em maiúsculas e minúsculas .
+A primeira palavra dentro da saída deve estar em maiúscula somente se a palavra original estiver em maiúscula
+(conhecido como Upper Camel Case, também conhecido como Pascal case).
+As próximas palavras devem ser sempre maiúsculas.
+
+def to_camel_case(str)
+
+end
